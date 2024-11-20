713c6df964a0bb9771b5c53395af07442f9566d6
Beecher Baker <beecher@beechersoftware.com>
2024-11-19 21:38:34 -0500

    delete dead code

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -3,8 +3,6 @@ import { ClientMain } from "@/app/ClientMain"
 export default function Home() {
     return (
         <>
-            {/*<h1 className="text-5xl font-bold">{`${metadata.title}`}</h1>*/}
-            {/*<em className="text-3xl">{metadata.description}</em>*/}
             <ClientMain
                 mapboxAccessToken={process.env.MAPBOX_TOKEN!}
                 debug={Boolean(process.env.DEBUG)}
